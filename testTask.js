722a7ab7c1b8b3aee77f96299b8dd877fe5ec8a6
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-07-10 13:00:53 +0300

    position of the mark is changed

--- testTask.js
+++ testTask.js
@@ -2,13 +2,13 @@ function counter(){
     var obj=document.getElementsByClassName('number')[0];
 
     obj.innerHTML= parseInt(obj.innerHTML) + 5 +"%";
-    if(obj.innerHTML=="100%")
-        {setTimeout(function(){},100);}
+    if(obj.innerHTML=="35%")
+        {setTimeout(function(){},200);}
     else
-    {setTimeout(counter,100);}
+    {setTimeout(counter,200);}
    }
 
-   setTimeout(counter,100);
+   setTimeout(counter,200);
 
 var arrowOnStart = 0;
 
@@ -20,9 +20,9 @@ function arrowMove() {
   obj.style.transform = 'rotate(' + arrowOnStart + 'deg)';
   
    if(arrowOnStart==120)
-        {setTimeout(function(){},100);}
+        {setTimeout(function(){},200);}
     else
-    {setTimeout(arrowMove,100);}
+    {setTimeout(arrowMove,200);}
    }
 
-   setTimeout(arrowMove,100);
+   setTimeout(arrowMove,200);
